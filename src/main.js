5ff8072245677b413bf37ea3016a3936ac223e77
hhdMrLion <echo_dyc@163.com>
2020-05-27 16:53:34 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -8,7 +8,13 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
+// 配置基础url地址
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 添加token到请求头中
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Welcome from "../components/Welcome";
+import Users from "../components/user/Users";
 
 Vue.use(VueRouter)
 
@@ -16,7 +18,18 @@ Vue.use(VueRouter)
     },
     {
       path: '/home',
-      component: Home
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          component: Welcome
+        },
+        {
+          path: '/users',
+          component: Users
+        }
+      ]
     }
 ]
 
